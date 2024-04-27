a60c3d907b10e21b55a74b5a199f27a323567ce6
Dragoon Dorise <rodrigosedano@me.com>
2024-04-27 21:29:36 +0200

    force debug

--- src/main/main.ts
+++ src/main/main.ts
@@ -1375,6 +1375,7 @@ if (process.env.NODE_ENV === 'production') {
 
 const isDebug =
   process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
+require('electron-debug')();
 
 if (isDebug) {
   require('electron-debug')();
